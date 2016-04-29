0585f83de6e4f6962b7446e020223dade07ae133
Tony Nguyen <tony@hub102.com>
2016-04-29 14:58:51 +0700

    add fb login

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '234248326940149',
+    secret: 'c4722aced600fa9d704b6ab0c2bda4d6'
+});
